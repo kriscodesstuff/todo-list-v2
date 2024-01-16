4fbcf2efd4a341d3525a74f47ac29ba610cab217
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2024-01-16 11:39:42 -0600

    Create allTasks tab

---
+++ js_modules/allTasks.js
@@ -0,0 +1,12 @@
+const allTasks = () => {
+    const content = document.querySelector('#content');
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+
+    h1.innerText = 'All Tasks';
+    p.innerText = 'No tasks added';
+
+    content.appendChild(h1);
+    content.appendChild(p);
+
+}

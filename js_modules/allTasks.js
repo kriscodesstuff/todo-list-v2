15872bae47848d9bcd4298738f947d8d16c9678a
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2024-01-18 15:02:10 -0600

    Load today tab when clicked and remove all tasks content

--- js_modules/allTasks.js
+++ js_modules/allTasks.js
@@ -1,13 +1,16 @@
 const allTasks = () => {
     const content = document.querySelector('#content');
+    const div = document.createElement('div');
     const h1 = document.createElement('h1');
     const p = document.createElement('p');
 
+    div.setAttribute('id', 'all-tasks-content');
     h1.innerText = 'All Tasks';
     p.innerText = 'No tasks added';
 
-    content.appendChild(h1);
-    content.appendChild(p);
+    content.appendChild(div);
+    div.appendChild(h1);
+    div.appendChild(p);
 
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import { today } from "../js_modules/today";
 
 const todayTab = document.querySelector('#today-tab');
 
-
 let loadHomePage = true;
 
 if(loadHomePage){
     allTasks();
 }
+
+todayTab.addEventListener('click', () => {
+    document.querySelector('#all-tasks-content').remove();
+    today();
+})
 

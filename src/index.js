19894e9d42ef9c04a5e0584e61aef296277f7cec
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2024-02-28 12:10:29 -0600

    Create function that sets task info to local storage

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const upcomingTab = document.querySelector('#upcoming-tab');
 const content = document.querySelector('#content');
 const addTaskBtn = document.querySelector('#add-task');
 const addTaskModal = document.querySelector('.add-task-modal');
+const addTaskModalBtn = document.querySelector('#add-task-modal-button');
 
 const loadDefaultPage = (function() {
     let loadHomePage = true;
@@ -51,5 +52,14 @@ const addTask = () => {
     addTaskModal.classList.remove('hidden');
 }
 
+const getTaskInfo = () => {
+    const taskName = document.getElementById('name').value;
+    const taskDescription = document.getElementById('description').value
+    localStorage.setItem("title", taskName);
+    localStorage.setItem("description", taskDescription);
+    addTaskModal.classList.add('hidden');
+}
+
 
 addTaskBtn.addEventListener('click', addTask);
+addTaskModalBtn.addEventListener('click',getTaskInfo);
